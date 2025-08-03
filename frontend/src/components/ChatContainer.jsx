1a12011e8d73a637fe0319955f78851a493d335b
Vedant <vedantkulkarni@vedants-MacBook-Air.local>
2025-08-03 15:51:27 +0530

    resolved the send image issue

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -96,7 +96,7 @@ const ChatContainer = () => {
                  type="text" placeholder='Send a message' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
                 <input onChange={handleSendImage} type="file" id='image' accept='image/png, image/jpeg' hidden />
                 <label htmlFor="image">
-                    <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer' />
+                    <img src={assets.gallery_icon} alt="" className='w-10 mr-2 cursor-pointer' />
                 </label>
             </div>
             <img onClick={handleSendMessage} src={assets.send_button} alt="" className='w-7 cursor-pointer'/>
